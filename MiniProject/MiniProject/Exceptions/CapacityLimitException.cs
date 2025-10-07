6e0327da4a9de9381b0ff010a2986704ca0b1e7f
amil740 <amilshm@code.edu.az>
2025-10-08 00:15:32 +0400

    mini proyekt

---
+++ MiniProject/MiniProject/Exceptions/CapacityLimitException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject.Exceptions
+{
+    internal class CapacityLimitException:Exception
+    {
+        public CapacityLimitException(string message) : base(message) { }   
+    }
+}
---
+++ MiniProject/MiniProject/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject.Exceptions
+{
+    internal class NotFoundException: Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
