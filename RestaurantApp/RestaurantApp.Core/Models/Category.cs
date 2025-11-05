0177da7034a28f3dd8b34d946216bc1cb38895b8
amil740 <amilshm@code.edu.az>
2025-11-05 17:31:32 +0400

    Add RestaurantApp project

--- RestaurantApp/RestaurantApp.Core/Models/Category.cs
+++ RestaurantApp/RestaurantApp.Core/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantApp.Core.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace RestaurantApp.Core.Models
 {
-    public class Category:Common.BaseEntity
+    public class Category:BaseEntity
     {
         public string Name { get; set; }
         public List<MenuItem> MenuItems { get; set; }
